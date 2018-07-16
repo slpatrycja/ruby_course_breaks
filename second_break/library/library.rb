a4a42fb26146281425a876bc96a42a12f20dda8a
slpatrycja <sl.patrycja.of@gmail.com>
2018-07-16 09:43:44 +0200

    corrections

--- second_break/library/library.rb
+++ second_break/library/library.rb
@@ -3,37 +3,33 @@ require "yaml"
 class Library
 	include Enumerable
 	attr_reader :books, :users_base
+
 	def initialize
 		@books = []
 		@users_base = []
+
 		$/="\n\n"
+		File.open("users.txt", "r").each { |object| @users_base << YAML::load(object) }
 
-		File.open("users.txt", "r").each do |object|
-  			@users_base << YAML::load(object)
-  		end
-
-  		contents = File.read("books.txt")
-  		if contents.empty?
+  		if File.read("books.txt").empty?
   			@books = []
   		else
-			@books = Kernel.eval(contents)
+			@books = Kernel.eval(File.read("books.txt"))
 		end
+
 	end
 
 	def add_book(book_title) 
 		@books.push({:title => book_title, :status => 'avaible'})
-		File.delete('books.txt')
-		f = open('books.txt', 'a') 
-  		f.write @books
-
+		update_books_file
 	end
 
 	def add_user(user)
 		@users_base.push(user)
-		file = File.open("users.txt", "w")
+		users_file = File.open("users.txt", "w")
 		@users_base.each do |user|
-    	file.puts YAML::dump(user)
-    	file.puts ""
+	    	users_file.puts YAML::dump(user)
+	    	users_file.puts ""
     	end
   	end
  
@@ -50,7 +46,7 @@ class Library
 			puts "Nie ma takiego użytkownika"
 			exit 
 		end
-		return user
+		user
 	end
 
 	def check_out(title)
@@ -58,34 +54,41 @@ class Library
 
 		if book_index == nil 
 			"This book is unavaible"
-			return 0
+			0
 		else
-			puts book_index
 			to_check_out = @books[book_index]
+
 			@books.delete_at(book_index)
 			to_check_out[:status] = 'unavaible'
 			@books.push(to_check_out)
-			File.delete('books.txt')
-			f = open('books.txt', 'a') 
-	  		f.write @books
-	  		return 1
+
+			update_books_file
+	  		1
   		end
 	end
 
 	def return_book(title)
 		to_return = @books.detect { |book| book[:title] == title }
+
 		@books.delete(to_return)
 		to_return[:status] = 'avaible'
 		@books.push(to_return)
-		File.delete('books.txt')
-		f = open('books.txt', 'a') 
-  		f.write @books
+
+		update_books_file
 	end
+
+	def update_books_file
+		File.delete('books.txt')
+		books_file = open('books.txt', 'a') 
+  		books_file.write @books
+	end
+
 end
 
 class User
 	@@all_cards = []
 	attr_reader :card_number, :user_books, :name, :history_of_checkouts
+
 	def initialize(name)
 		@card_number = get_card_number
 		@name = name
@@ -105,8 +108,10 @@ class User
 
 	def check_out(title, library)
 		if library.check_out(title) == 1
+
 			@user_books.push(title)
 			@history_of_checkouts.push({ :title => title, :date => Time.now})
+
 			library.users_base.delete(self)
 			library.add_user(self)
 			return 1
@@ -117,6 +122,7 @@ class User
 	def return_book(title, library)
 		@user_books.delete(title)
 		@history_of_checkouts.delete(@history_of_checkouts.detect { |book| book[:title] == title })
+
 		library.return_book(title)
 		library.users_base.delete(self)
 		library.add_user(self)
@@ -135,27 +141,28 @@ end
 def menu(library)
 	
 	puts "Menu:"
-	puts "1. Show the list of our books", "2. Give us a book", "3. Add yourself to readers register and create your own librarian card"
+	puts "1. Show the list of our books", "2. Give us a book"
+	puts "3. Add yourself to readers register and create your own librarian card"
 	puts "4. Log in", "5. Exit"
 
 	case gets.to_i
 		when 1 
 			library.show_books
-			exit_or_return(library)
+			exit_or_return_to_menu(library)
 		when 2	
 			puts "Please enter the title of the book you want to give"
 			title = gets.chomp.downcase
 			library.add_book(title)
 			library.show_books
 			puts "Thank you for your donation"
-			exit_or_return(library)
+			exit_or_return_to_menu(library)
 		when 3
 			puts "Please type your name and lastname"
 			name = gets.chomp.downcase
 			new_user = User.new(name)
 			library.add_user(new_user)
 			puts "Thanks for joining us!"
-			exit_or_return(library)
+			exit_or_return_to_menu(library)
 			
 		when 4
 			
@@ -172,35 +179,44 @@ end
 
 def user_menu(library, user)
 	puts "What do you want to do?"
-			puts "1. Check out the book", "2. Return the book", "3. Show history of checkouts"
-				case gets.to_i
-				when 1
-					puts "Please enter the title of the book you want to check out"
-					title = gets.chomp.downcase
-					result = user.check_out(title, library)
-					if result == 1
-						puts "Here's the fresh list of books that you've checked out"
-						puts user.user_books
-					
-					else puts "This book is unavaible"
-					end
-					user_exit_or_return(library, user)
-				when 2
-					puts "Please enter the title of the book you want to return"
-					title = gets.chomp.downcase
-					user.return_book(title, library)
+	puts "1. Check out the book", "2. Return the book", "3. Show history of checkouts", "4. Log out"
+		case gets.to_i
+			when 1
+				puts "Please enter the title of the book you want to check out"
+				title = gets.chomp.downcase
+				result = user.check_out(title, library)
+
+				if result == 1
 					puts "Here's the fresh list of books that you've checked out"
 					puts user.user_books
-					user_exit_or_return(library, user)
-
-				when 3
-					puts "Your history:"
-					user.history_of_checkouts.each { |book| puts "Title: #{book[:title]}, date: #{book[:date]}" }
-					user_exit_or_return(library, user)
+					
+				else puts "This book is unavaible"
 				end
+
+				exit_or_return_to_user_menu(library, user)
+			when 2
+				puts "Please enter the title of the book you want to return"
+				title = gets.chomp.downcase
+				user.return_book(title, library)
+
+				puts "Here's the fresh list of books that you've checked out"
+				puts user.user_books
+
+				exit_or_return_to_user_menu(library, user)
+
+			when 3
+				puts "Your history:"
+				user.history_of_checkouts.each { |book| puts "Title: #{book[:title]}, date: #{book[:date]}" }
+				exit_or_return_to_user_menu(library, user)
+			
+
+		    when 4
+		    	user = nil
+		    	menu(library)
+		end
 end
 
-def exit_or_return(library)
+def exit_or_return_to_menu(library)
 	puts "Press 1 to quit or 2 to go back to menu"
 	case gets.to_i
 		when 1 then exit
@@ -208,7 +224,7 @@ def exit_or_return(library)
 	end
 end
 
-def user_exit_or_return(library, user)
+def exit_or_return_to_user_menu(library, user)
 	puts "Press 1 to quit or 2 to go back to user panel"
 	case gets.to_i
 		when 1 then exit
